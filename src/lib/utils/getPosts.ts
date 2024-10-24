3e1d11d3ddfb78edeb615713aef1e63bb152f2fc
David (davru) Sanchez <contact@dsgdsr.me>
2024-10-24 18:13:18 +0200

    [feature] Work page skeleton

--- src/lib/utils/getPosts.ts
+++
@@ -1,6 +0,0 @@
-import type { BlogPost } from '@lib/models';
-import data from '../../../public/posts.json';
-
-export const getPosts = (): BlogPost[] => {
-  return (data as unknown as BlogPost[]) ?? [];
-};
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,3 +1,2 @@
-export * from './getPosts';
 export * from './meta';
 export * from './relativeTime';
